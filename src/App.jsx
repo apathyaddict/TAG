90fbd200bdae550614a8c5fd5f8a08a5b964859a
eve seni <senieve@gmail.com>
2024-07-02 20:12:29 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import CreatePage from "./Pages/CreatePage";
 import ListPage from "./Pages/ListPage";
