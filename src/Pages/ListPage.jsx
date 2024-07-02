32e950b99f44141b23fbbc4137884594a1a944c5
eve seni <senieve@gmail.com>
2024-07-02 08:42:03 +0200

    pagination set up

--- src/Pages/ListPage.jsx
+++ src/Pages/ListPage.jsx
@@ -1,38 +1,99 @@
 import React, { useEffect, useState } from "react";
 import RestaurantList from "../components/RestaurantList.jsx";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from "firebase/firestore";
 import { db } from "../firebase.js";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { FaSpinner } from "react-icons/fa";
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
 
 const ListPage = ({ editSelectedRestaurant }) => {
   const [allRestaurants, setAllRestaurants] = useState([]);
-  const [loading, setLoading] = useState(true); // Initialize loading state as true
+  const [loading, setLoading] = useState(false);
+  const [lastVisible, setLastVisible] = useState(null);
+  const [moreLoading, setMoreLoading] = useState(false);
 
   useEffect(() => {
     const fetchRestaurants = async () => {
+      setLoading(true);
       try {
-        const querySnapshot = await getDocs(collection(db, "fiches"));
-        const restaurants = querySnapshot.docs.map((doc) => ({
+        const first = query(
+          collection(db, "fiches"),
+          orderBy("name"),
+          limit(4)
+        );
+
+        const documentSnapshots = await getDocs(first);
+
+        const lastVisibleDoc =
+          documentSnapshots.docs[documentSnapshots.docs.length - 1];
+        setLastVisible(lastVisibleDoc);
+
+        const restaurants = documentSnapshots.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
+
         setAllRestaurants(restaurants);
-        setLoading(false); // Set loading to false after data is fetched
-        console.log("Fetched restaurants:", restaurants); // Log fetched data
       } catch (error) {
         console.error("Error fetching documents: ", error);
-        setLoading(false); // Ensure loading is set to false on error
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchRestaurants();
   }, []);
 
+  const fetchMoreRestaurants = async () => {
+    if (!lastVisible) return;
+
+    setMoreLoading(true);
+    try {
+      const next = query(
+        collection(db, "fiches"),
+        orderBy("name"),
+        startAfter(lastVisible),
+        limit(4)
+      );
+
+      const documentSnapshots = await getDocs(next);
+
+      if (documentSnapshots.docs.length === 0) {
+        setLastVisible(null); // No more documents
+        return;
+      }
+
+      const lastVisibleDoc =
+        documentSnapshots.docs[documentSnapshots.docs.length - 1];
+      setLastVisible(lastVisibleDoc);
+
+      const moreRestaurants = documentSnapshots.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setAllRestaurants((prevRestaurants) => [
+        ...prevRestaurants,
+        ...moreRestaurants,
+      ]);
+    } catch (error) {
+      console.error("Error fetching more documents: ", error);
+    } finally {
+      setMoreLoading(false);
+    }
+  };
+
   if (loading) {
     return (
-      <div className="max-w-full mr-10 p-10">
-        <Skeleton height={100} className="my-2" count={3} />
+      <div className=" flex justify-center align-middle mx-auto mr-10 p-10">
+        <FaSpinner className="animate-spin h-16 w-16" />
       </div>
     );
   }
@@ -43,6 +104,15 @@ const ListPage = ({ editSelectedRestaurant }) => {
         restaurants={allRestaurants}
         {...{ editSelectedRestaurant }}
       />
+
+      <div className="text-center my-10 capitalize">
+        <button
+          onClick={fetchMoreRestaurants}
+          className="px-10 py-2 bg-white text-stone-800 border-stone-200 hover:text-gray-600 focus:text-gray-800 font-bold border uppercase rounded-md disabled:text-stone-300 hover:cursor-auto"
+          disabled={moreLoading || !lastVisible}>
+          {moreLoading ? "Chargement..." : "Charger plus"}
+        </button>
+      </div>
     </div>
   );
 };
